6703b0516e29c0828eec8a694dd5fb2873654562
yan <yanclopes08@gmail.com>
2026-03-24 19:43:11 -0300

    feat: Deploy to docker hub action

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    app.enableCors({
+        origin: '*', // ou o domínio exato do seu front-end
+        methods: ['GET', 'POST', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/link/dto/CreateLinkDto.ts
+++ src/modules/link/dto/CreateLinkDto.ts
@@ -1,6 +1,4 @@
 import { IsString, IsOptional, IsDateString } from 'class-validator';
-import { Transform } from 'class-transformer';
-import { randomBytes } from 'crypto';
 
 export class CreateLinkDto {
   @IsString()
