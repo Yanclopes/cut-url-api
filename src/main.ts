a6e33db4e78940f62fa812a09ab4dd1aadc4dd93
Yanclopes <91619524+Yanclopes@users.noreply.github.com>
2026-03-27 20:48:27 -0300

    feat: prefix api

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,23 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    const config = new DocumentBuilder()
-        .setTitle('Cut URL API')
-        .setDescription('API para encurtamento de URLs')
-        .setVersion('1.0')
-        .addTag('links')
-        .build();
+  const config = new DocumentBuilder()
+      .setTitle('Cut URL API')
+      .setDescription('API para encurtamento de URLs')
+      .setVersion('1.0')
+      .addTag('links')
+      .build();
+  app.setGlobalPrefix('api')
 
-    const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config);
 
-    SwaggerModule.setup('docs', app, document);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-    app.enableCors({
-        origin: '*',
-        methods: ['GET', 'POST', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-    });
+  SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.enableCors({
+      origin: '*',
+      methods: ['GET', 'POST', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
